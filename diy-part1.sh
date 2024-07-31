ed7adbece59dd4385b12d8246c3454323440703b
NALGNIBIHZGNEM <772097880@qq.com>
2024-07-31 23:51:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/NALGNIBIHZGNEM/immortalWrt-packages' >> feeds.conf.default
